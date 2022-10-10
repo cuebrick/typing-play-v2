0983bec3f63cb0b1498030589b44167adfa1865d
LSM <lsm4551@on.mjc.ac.kr>
2022-10-10 18:01:50 +0900

    change button to Link.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import styles from 'assets/styles/index-page.module.scss'
+import Link from "next/link";
 
 function IndexPage(): JSX.Element {
     return (
@@ -7,9 +8,9 @@ function IndexPage(): JSX.Element {
                 <p>
                     아래 과정 중 하나를 선택하세요.
                 </p>
-                <button>
+                <Link href="/user">
                     사용자 등록
-                </button>
+                </Link>
             </div>
         </div>
     )
