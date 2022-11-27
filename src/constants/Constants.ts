b399d4bec3b0dce2bf2c2536981a16d4eda2dde5
LSM <lsm4551@on.mjc.ac.kr>
2022-11-27 20:11:02 +0900

    add constants.

---
+++ src/constants/Constants.ts
@@ -0,0 +1,29 @@
+export const languageOptions = [
+  {
+    value: 'ko',
+    label: '한글'
+  },
+  {
+    value: 'en',
+    label: '영문'
+  }
+];
+
+export const inputType = {
+  word: {
+    value: 'word',
+    label: '단어'
+  },
+  letter: {
+    value: 'letter',
+    label:'자소'
+  }
+}
+
+
+const Constants = {
+  key: 'some key',
+  langOpt: languageOptions,
+}
+
+export default Constants;
