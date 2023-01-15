5ec186003e9513e9c78482c70569a370e8bcec7a
LSM <lsm4551@on.mjc.ac.kr>
2023-01-15 18:03:03 +0900

    check current signed in.

--- src/components/layout/DefaultLayout.tsx
+++ src/components/layout/DefaultLayout.tsx
@@ -1,26 +1,38 @@
-import {ReactNode} from "react";
+import {ReactNode, useEffect, useState} from "react";
 import Image from "next/image"
 import logo from "assets/images/logo.svg"
 import userIcon from "assets/images/user-icon.svg"
+import {auth} from "database";
 
-type Props = {children: ReactNode}
+type Props = { children: ReactNode }
 
 function DefaultLayout({children}: Props): JSX.Element {
-    return (
-        <>
-            <header className="default-layout-header">
-                <div className="logo">
-                    <Image src={logo} alt="Typing Play"/>
-                </div>
-                <div className="user-info">
-                    <Image src={userIcon} alt="User Icon"/>
-                </div>
-            </header>
-            <div className="contents-body">
-                {children}
-            </div>
-        </>
-    )
+  const [loginUser, setLoginUser] = useState(auth.currentUser)
+  useEffect(() => {
+    auth.onAuthStateChanged((user) => {
+      if (user) {
+        console.log('user는 >>>', user, auth)
+        setLoginUser(user)
+      }
+    })
+  }, [auth])
+
+  return (
+    <>
+      <header className="default-layout-header">
+        <div className="logo">
+          <Image src={logo} alt="Typing Play"/>
+        </div>
+        <div className="user-info">
+          {loginUser?.email}
+          <Image src={userIcon} alt="User Icon"/>
+        </div>
+      </header>
+      <div className="contents-body">
+        {children}
+      </div>
+    </>
+  )
 }
 
 export default DefaultLayout;
