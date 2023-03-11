54fc9e1cb43b6b0dbf18ccac17a849f7f69c8f70
cuebrick <cuebrick@gmail.com>
2023-03-11 11:47:53 +0900

    Add route handling in WelcomeCardItem.tsx

--- src/components/pages/WelcomeCardItem.tsx
+++ src/components/pages/WelcomeCardItem.tsx
@@ -1,8 +1,23 @@
+import {useRouter} from 'next/router';
+
 function WelcomeCardItem(): JSX.Element {
-    return (
-        <div className="welcome-card-item">
-            타플은 한글 타자 연습을 위한 프로그램 입니다.
-        </div>
-    )
+  const router = useRouter();
+
+  const onClickLevelList = (): void => {
+    router.push('/levels')
+  };
+
+  const onClickContinue = (): void => {
+    // TODO: 사용자 정보로부터 이전 레벨 정보를 가져와 -> router 이동
+  }
+
+  return (
+    <div className="welcome-card-item">
+      타플은 한글 타자 연습을 위한 프로그램 입니다.
+      <button onClick={onClickLevelList}>목록에서 시작</button>
+      <button onClick={onClickContinue}>이어서 시작</button>
+    </div>
+  );
 }
-export default WelcomeCardItem
+
+export default WelcomeCardItem;
