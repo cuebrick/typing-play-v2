3f2036bf3507bb9e5e8c2e98a30d02bf737dbea1
LSM <lsm4551@on.mjc.ac.kr>
2023-04-16 21:09:12 +0900

    Remove unused code.

--- src/pages/levels/[levelId].tsx
+++ src/pages/levels/[levelId].tsx
@@ -15,10 +15,9 @@ function LevelsIdPage(): JSX.Element {
   const store = useContext(LevelContext);
   // const [levelData, setLevelData] = useState<ILevel>();
   const [nextKey, onChangeKeyInputList] = useNextLetter();
-  const [nextCode, setNextCode] = useState<string>('');
+  // const [nextCode, setNextCode] = useState<string>('');
   const [hangulMode, setHangulMode] = useState<boolean>(true);
 
-  // use getLevel fn
   useEffect(() => {
     if (levelId) {
       store.getLevel(levelId as string);
@@ -33,11 +32,11 @@ function LevelsIdPage(): JSX.Element {
 
   const [keyInputList, keyInput] = useKeyboardInput();
 
-  useEffect(() => {
-    if (nextKey) {
-      setNextCode(getCode(nextKey));
-    }
-  }, [nextKey]);
+  // useEffect(() => {
+  //   if (nextKey) {
+  //     setNextCode(getCode(nextKey));
+  //   }
+  // }, [nextKey]);
 
   useEffect(() => {
     onChangeKeyInputList(keyInputList);
@@ -49,15 +48,15 @@ function LevelsIdPage(): JSX.Element {
     }
   }, [keyInput]);
 
-  function getCode(alphabet: string) {
-    for (let key in Keymap) {
-      let keymap = Keymap as any;
-      if (keymap[key].krn === alphabet) {
-        return keymap[key].enn;
-      }
-    }
-    return '';
-  }
+  // function getCode(alphabet: string) {
+  //   for (let key in Keymap) {
+  //     let keymap = Keymap as any;
+  //     if (keymap[key].krn === alphabet) {
+  //       return keymap[key].enn;
+  //     }
+  //   }
+  //   return '';
+  // }
 
   return (
     <div className="typing-level">
