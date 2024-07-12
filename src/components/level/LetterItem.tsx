1b7b56ca36f3010dd85b40592b5ada62fa4fea3f
2sangmin <rkdmf7220@gmail.com>
2024-07-12 18:19:42 +0900

    Fix typo checking timing so that it works when typing the next text.

--- src/components/level/LetterItem.tsx
+++ src/components/level/LetterItem.tsx
@@ -1,4 +1,4 @@
-import {useEffect, useState} from 'react';
+import {useEffect, useRef, useState} from 'react';
 import Hangul from 'korean-js/src/hangul';
 import {ILetter} from 'interfaces/LevelInterface';
 import clsx from 'clsx';
@@ -6,12 +6,16 @@ import clsx from 'clsx';
 interface IProps {
   data: ILetter;
   active: boolean;
+  isModify: boolean;
+  itemIndex: number;
+  currentIndex: number;
 }
 
-function LetterItem({data, active}: IProps): JSX.Element {
+function LetterItem({data, active, isModify, itemIndex, currentIndex}: IProps): JSX.Element {
   const [assembledSampleText, setAssembledSampleText] = useState<string>();
   const [assembledTypingText, setAssembledTypingText] = useState<string>();
   const [correctTypo, setCorrectTypo] = useState<string>();
+  const isReadyRef = useRef(false);
 
   useEffect(() => {
     if (data.sampleText) {
@@ -28,19 +32,29 @@ function LetterItem({data, active}: IProps): JSX.Element {
       // 빈 텍스트는 undefined라 else에서 빈 string으로 초기화
       setAssembledTypingText('');
     }
-    const result = JSON.stringify(data.sampleText) === JSON.stringify(data.typingText) ? 'correct' : 'incorrect';
-    if (data.typingText?.length || data.isModify) {
-      // 자소 지우기                 // 글자 지우기 (글자 지울 시 isModify = true가 꼭 들어감)
-      setCorrectTypo(result);
+  }, [data, data.typingText, active, data.sampleText, isModify]);
+
+  useEffect(() => {
+    if (itemIndex === currentIndex || itemIndex === currentIndex - 1) {
+      // todo: 모종의 이유로 -1을 넣었으나 해당 이유 까먹음.
+      isReadyRef.current = true;
     }
-  }, [data, data.typingText, active, data.sampleText]);
+    if (isReadyRef && itemIndex !== currentIndex) {
+      isReadyRef.current = false;
+      const result = JSON.stringify(data.sampleText) === JSON.stringify(data.typingText) ? 'correct' : 'incorrect';
+      if (data.typingText?.length || isModify) {
+        // 자소 지우기                 // 글자 지우기 (글자 지울 시 isModify = true가 꼭 들어감)
+        setCorrectTypo(result);
+      }
+    }
+  }, [currentIndex, itemIndex]);
 
   return (
     <div
       className={clsx('letter-item', {active}, [
         {incorrect: correctTypo === 'incorrect'},
-        {isModify: data.isModify && correctTypo === 'correct'},
-        {correct: !data.isModify && correctTypo === 'correct'}
+        {isModify: isModify && correctTypo === 'correct'},
+        {correct: !isModify && correctTypo === 'correct'}
       ])}
     >
       <span className="token">{assembledSampleText}</span>
