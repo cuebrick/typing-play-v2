fe56e0d547f2caffa100834a41f8c75fb64a1047
2sangmin <rkdmf7220@gmail.com>
2024-08-31 23:12:11 +0900

    Apply styled-components to card item.

--- src/components/CardItem.tsx
+++ src/components/CardItem.tsx
@@ -1,9 +1,19 @@
+import styled from 'styled-components';
+
+const Container = styled.div`
+  background-color: #fff;
+  box-shadow: 0 1px 10px rgba(0, 0, 0, 0.15);
+`;
+const CardHeader = styled.div``;
+const CardBody = styled.div``;
+
 function CardItem({title, children, width, height}: any): JSX.Element {
   return (
-    <div className="card-item" style={{width, height}}>
-      <div className="header">{title}</div>
-      <div className="body">{children}</div>
-    </div>
+    <Container style={{width, height}}>
+      <CardHeader>{title}</CardHeader>
+      <CardBody>{children}</CardBody>
+    </Container>
   );
 }
+
 export default CardItem;
