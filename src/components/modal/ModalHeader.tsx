36f0a87ca5cc40b55fbb1be1f98b5430940dbeec
2sangmin <rkdmf7220@gmail.com>
2024-08-31 23:09:59 +0900

    Apply styled-components to modal.

--- src/components/modal/ModalHeader.tsx
+++ src/components/modal/ModalHeader.tsx
@@ -1,4 +1,21 @@
-import {ReactNode} from "react";
+import {ReactNode} from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  height: 40px;
+  padding: 10px 20px;
+  border-bottom: 1px solid #dddddd;
+  display: flex;
+
+  > h3 {
+    margin: 0;
+    font-size: 16px;
+    font-weight: normal;
+  }
+`;
+const CloseButton = styled.button`
+  margin-left: auto;
+`;
 
 interface IProps {
   children: ReactNode;
@@ -7,15 +24,15 @@ interface IProps {
 
 function ModalHeader({children, onClose}: IProps): JSX.Element {
   return (
-      <div className="modal-header">
-        <h3>{children}</h3>
-        <button className="close-button" onClick={onClose}>x</button>
-      </div>
-  )
+    <Container className="modal-header">
+      <h3>{children}</h3>
+      <CloseButton onClick={onClose}>x</CloseButton>
+    </Container>
+  );
 }
 
 ModalHeader.defaultProps = {
-  title: '알림'
-}
+  // title: '알림'
+};
 
 export default ModalHeader;
