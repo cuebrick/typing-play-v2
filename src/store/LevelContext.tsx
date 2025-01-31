c7dd34dc034440f00f8d19f9e03b701590def6c5
2sangmin <rkdmf7220@gmail.com>
2025-01-31 17:01:41 +0900

    Add comment.

--- src/store/LevelContext.tsx
+++ src/store/LevelContext.tsx
@@ -24,9 +24,9 @@ import {AuthContext} from './AuthContext';
 export interface ILevelContext {
   checkedApp: boolean;
   getAppInfo(): Promise<IAppInfo>;
+  getCategoryList(): void;
   getLevelList(): void;
   getLevelData(params: ILevelListParams): Promise<ILevel[]>;
-  getCategoryList(): void;
   checkAppVersion(): void;
   saveUserTypingData(userTypingData: IUserTypingData): void;
 }
@@ -39,6 +39,28 @@ const defaultState: ILevelContext = {
     return docSnap.data() as IAppInfo;
   },
 
+  /**
+   * 타자 연습의 카테고리를 불러옴.
+   * 타자 레벨을 불러와 정렬할 때 이 카테고리에 맞게 정렬함.
+   * 이를 위해 레벨보다 카테고리 불러오기가 선생되어야 함.
+   */
+  async getCategoryList() {
+    const q = query(collection(db, 'categories'), orderBy('order'));
+    const querySnapshot: QuerySnapshot = await getDocs(q);
+    const list: ICategory[] = [];
+    querySnapshot.forEach((docObj) => {
+      list.push({...docObj.data(), id: docObj.id} as ICategory);
+    });
+    runInAction(() => {
+      localStorage.setItem('categoryList', JSON.stringify(list));
+    });
+  },
+
+  /**
+   * 타자 연습을 할 레벨들을 불러옴.
+   * getCategoryList 실행 후 이 함수가 실행됨.
+   * 각 카테고리 id를 이용해 해당하는 레벨들을 불러옴.
+   */
   async getLevelList() {
     const categoryList = JSON.parse(localStorage.getItem('categoryList') as string);
 
@@ -78,6 +100,9 @@ const defaultState: ILevelContext = {
     localStorage.setItem('levelList', JSON.stringify(levelList));
   },
 
+  /**
+   * 조건에 부합하는 타자 레벨을 불러옴.
+   */
   async getLevelData(params: ILevelListParams) {
     const qc = []; // QueryConstraint(s)
     if (params?.categoryId) {
@@ -97,18 +122,11 @@ const defaultState: ILevelContext = {
     return list;
   },
 
-  async getCategoryList() {
-    const q = query(collection(db, 'categories'), orderBy('order'));
-    const querySnapshot: QuerySnapshot = await getDocs(q);
-    const list: ICategory[] = [];
-    querySnapshot.forEach((docObj) => {
-      list.push({...docObj.data(), id: docObj.id} as ICategory);
-    });
-    runInAction(() => {
-      localStorage.setItem('categoryList', JSON.stringify(list));
-    });
-  },
-
+  /**
+   * 버전 체크.
+   * localStorage와 정보를 비교.
+   * 다를 경우 카테고리와 목록 다시 불러오고 저장.
+   */
   async checkAppVersion() {
     let appLocalInfo = {} as IAppInfo;
     const appServerInfo = await this.getAppInfo();
